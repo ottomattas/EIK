996fb8ea58b48dafaec3653e3a2123e1dccec88f
student <student@ubuntu-desktop1204.planet.zz>
2015-12-19 13:01:48 +0200

    v0.1 Poolik skript aine materjalidest lisatud Otto GIT reposse.

---
+++ I387_Skriptimiskeeled/kodutoo1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Otto Mättas
+#See skript jagab etteantud grupile uue kausta
+
+#Vers. tabel
+# v0.1 Poolik skript aine materjalidest lisatud Otto GIT reposse. Source: https://wiki.itcollege.ee/index.php/Bash_n%C3%A4ide
+ 
+#NB See skript on pooleli ja sisaldab mitmeid ebatäpsuseid
+#mõeldud alustamiseks ja parandamiseks
+ 
+export LC_ALL=C
+ 
+#Kontrollib, kas skript on käivitatud juurkasutajana
+if [ $UID -ne 0 ]
+then
+  echo "käivita skript $(basename $0) juurkasutaja õigustes"
+  exit 1
+fi
+ 
+ 
+#Kontrollib, kas on ette antud õige arv muutujaid
+ 
+if [ $# -eq 2 ]
+then
+  KAUST=$1
+  GRUPP=$2
+ 
+else 
+  if [ $# -eq 3 ]
+  then
+    KAUST=$1
+    GRUPP=$2
+    SHARE=$3
+  else
+    echo "kasuta sktipti $(basename $0) KAUST GRUPP [SHARE]"
+    exit 1
+  fi
+fi
+ 
+#Kontrollib, kas samba on paigaldatud (vajadusel paigaldab)
+type smbd > /dev/null 2>&1 
+ 
+if [ $? -ne 0 ]
+then
+  apt-get update > /dev/null 2>&1 && apt-get install samba -y || exit 1 
+fi
+ 
+#Kontrollib, kas kaust on olemas (vajadusel loob)
+test -d $KAUST || mkdir -p $KAUST
+ 
+ 
+ 
+#Kontrollib, kas grupp on olemas (vajadusel loob)
+getent group $GRUPP > /dev/null || addgroup $GRUPP  > /dev/null 
+ 
+ 
+#Siin jäi tunnis pooleli

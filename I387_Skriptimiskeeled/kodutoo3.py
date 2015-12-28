b69abe5b41d8a21231f11590d974b0c69c3540de
ottomattas <otto.mattas@gmail.com>
2015-12-28 13:11:37 +0200

    Initial add

---
+++ I387_Skriptimiskeeled/kodutoo3.py
@@ -0,0 +1,12 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+#Otto Mättas
+#See skript ...
+
+#Vers. tabel
+# v0.1 - ... 
+
+import sys
+
+#NB See skript on pooleli ja sisaldab mitmeid ebatäpsuseid
+#mõeldud alustamiseks ja parandamiseks

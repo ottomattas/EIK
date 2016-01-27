82bfbb118918703b7bdada900f884526cbdffc8d
0tto Mättas <otto.mattas@riigikogu.ee>
2016-01-27 19:10:44 +0200

    v0.1 Ülesande defineerimine

---
+++ I387_Skriptimiskeeled/kontrolltoo2.py
@@ -0,0 +1,13 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+#Otto Mättas
+#Skript, mis leiab sisendfailist inimeste nimed, ning annab tulemuse, milleks on nimest tuletatud EIKga seotud spetsiifiline info, väljundfailis.
+
+#Vers. tabel
+# v0.1 Ülesande defineerimine
+
+import sys
+
+
+#Tulemuse raport kasutajale
+print "Programm lõpetas töö"

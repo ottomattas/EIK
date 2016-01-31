77d80547b0785afb37e5646edb608af5f980db9f
ottomattas <otto.mattas@itcollege.ee>
2016-01-31 17:35:31 +0200

    v1.0 Valmistoode

--- I387_Skriptimiskeeled/kontrolltoo2.py
+++ I387_Skriptimiskeeled/kontrolltoo2.py
@@ -9,12 +9,16 @@
 # v0.1.2 Sisendfailist saame vähendatud listi
 # v0.1.3 Koodi vormingu seadmine
 # v0.2 Ees- ja perekonnanime leidmine lisatud
+# v1.0 Valmistoode
 
 import sys
+import random
+import string
+import csv
 
-print "################################################################################"
-print "###PROGRAMM EI TÖÖTA KAUGELTKI VEEL NII NAGU ÜLESANDE LÄHTETEKST SEDA SOOVIB!###"
-print "################################################################################"
+#print "################################################################################"
+#print "###PROGRAMM EI TÖÖTA KAUGELTKI VEEL NII NAGU ÜLESANDE LÄHTETEKST SEDA SOOVIB!###"
+#print "################################################################################"
 
 
 #Programm kontrollib argumentide arvu
@@ -33,22 +37,41 @@ try:
 			elem = elem[2:]
 			elem = elem.split (' ')
 			nimed = elem[:2]
+			#Leiame eesnime
 			eesnimi = nimed[0]
-			perekonnanimi = nimed[1] 
-#			print ("Eesnimi:", eesnimi, "Perekonnanimi:", perekonnanimi)
+			#Leiame perekonnanime
+			perekonnanimi = nimed[1]
+			#Leiame täisnime
+			taisnimi = eesnimi + " " + perekonnanimi
+			#Leiame kasutajanime (eesnime esitäht + perekonnanimi, max 8 tähemärki)
+			kasutajatunnus = eesnimi[:1].lower()+ perekonnanimi[:7].lower()
+			#Leiame e-maili aadressi 
+			email = eesnimi.lower() + "." + perekonnanimi.lower() + "@itcollege.ee"
+			#Leiame 20st suvalisest tähemärgist koosneva tokeni
+			token = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(20))
+			##################################################################
+			##################################################################
+			# Vali endale meelepärane meetod CSV failis väärtuste kuvamiseks:#
+			##################################################################
+			# 1) CSV faili väljastatav tulemus // täisnime variant (ühes väärtuses on mõlemad, nii ees- kui ka perekonnanimi)
+			# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+			tulemus = kasutajatunnus, taisnimi, email, token
+			#################################################################
+			# 2) CSV faili väljastatav tulemus // eraldi nimede variant (ühes väärtuses on eesnimi, teises perekonnanimi)
+			#tulemus = kasutajatunnus, eesnimi, perekonnanimi, email, token
+			#################################################################
+			#################################################################
+			try:
+				output_file = open(sys.argv[2], 'a')
+				valjund = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+				valjund.writerow(tulemus)
+				output_file.close()
+			except IOError:
+				print "Väljundfaili ei saa kirjutada..."
+				sys.exit(3)
 except IOError:
 	print "Sisendfaili ei saa avada..."
 	sys.exit(2)
-
-#Määra VÄLJUNDFAIL (lisaks kontroll selle faili avamise võimalikkusele)
-try:
-	output_file = open(sys.argv[2], 'w')
-#	print t2ht+perenimi','t2isnimi','eesnimi'.'perenimi'@itcollege.ee,'token
-	#Sulgeme VÄLJUNDFAILI
-#	output_file.close()
-except IOError:
-	print "Väljundfaili ei saa kirjutada..."
-	sys.exit(3)
 
 #Tulemuse raport kasutajale
 print "Programm lõpetas töö."

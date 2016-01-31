e42c9ca78a81603774571beeb0a0380be0208a1c
ottomattas <otto.mattas@itcollege.ee>
2016-01-31 16:12:08 +0200

    v0.2 Ees- ja perekonnanime leidmine lisatud

--- I387_Skriptimiskeeled/kontrolltoo2.py
+++ I387_Skriptimiskeeled/kontrolltoo2.py
@@ -8,6 +8,7 @@
 # v0.1.1 Kondikava ülespanek
 # v0.1.2 Sisendfailist saame vähendatud listi
 # v0.1.3 Koodi vormingu seadmine
+# v0.2 Ees- ja perekonnanime leidmine lisatud
 
 import sys
 
@@ -22,18 +23,19 @@ if len(sys.argv) != 3:
 	print "Kasuta programmi järgmiselt:", sys.argv[0], "[SISENDFAIL] [VÄLJUNDFAIL]"
 	sys.exit(1)
 
-#Määra SISENDFAIL (lisaks kontroll selle faili avamise võimalikkusele)
+#Ava SISENDFAIL listina ilma tühjade ridadeta (lisaks kontroll selle faili avamise võimalikkusele)
 try:
-	input_file = open(sys.argv[1], 'r')
-	#Leiame sisendfailist listid
-	for line in input_file:
-		#Eraldame listist vajaliku info
-		data = line.split(' ')
-		data = data[0:2]
-		#TESTPRINT
-		print data
-		#Sulgeme SISENDFAILI
-	input_file.close()
+	with open(sys.argv[1], 'r') as SISENDFAIL:
+		data = filter(None, (line.rstrip() for line in SISENDFAIL))
+		#Eraldame listist nimed
+		data = data[1:]
+		for elem in data:
+			elem = elem[2:]
+			elem = elem.split (' ')
+			nimed = elem[:2]
+			eesnimi = nimed[0]
+			perekonnanimi = nimed[1] 
+#			print ("Eesnimi:", eesnimi, "Perekonnanimi:", perekonnanimi)
 except IOError:
 	print "Sisendfaili ei saa avada..."
 	sys.exit(2)
@@ -43,7 +45,7 @@ try:
 	output_file = open(sys.argv[2], 'w')
 #	print t2ht+perenimi','t2isnimi','eesnimi'.'perenimi'@itcollege.ee,'token
 	#Sulgeme VÄLJUNDFAILI
-	output_file.close()
+#	output_file.close()
 except IOError:
 	print "Väljundfaili ei saa kirjutada..."
 	sys.exit(3)

9d2feae878f963fd41973918432b57266d23455c
ottomattas <otto.mattas@itcollege.ee>
2016-01-31 13:58:12 +0200

    v0.2 Lisatud failiotsing kasutajale kirjutamiseks kättesaadavate failide nimekirja printimiseks väljundfaili ning väljundfaili avamise kontrolli täiustus.

--- I387_Skriptimiskeeled/arvestustoo.sh
+++ I387_Skriptimiskeeled/arvestustoo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Otto Mättas
-#Skript, mis leiab sisendafailist otsitava ning annab tulemuse väljundfailis.
+#Skript, mis leiab etteantud kataloogist kõik failid, mida kasutaja saab kirjutamiseks avada ning esitab tulemuse väljundfailis.
 
 #Vers. tabel
 # v0.1 Skriptikäivituskontrollid on inspireeritud ainekursuse materjalidest. Source: https://wiki.itcollege.ee/index.php/Bash_n%C3%A4ide
-#
+# v0.2 Lisatud failiotsing kasutajale kirjutamiseks kättesaadavate failide nimekirja printimiseks väljundfaili ning väljundfaili avamise kontrolli täiustus.
 
 export LC_ALL=C
 
@@ -46,22 +46,40 @@ export LC_ALL=C
                 then
                         echo "Väljunfaili ei leitud,loome selle!"
 			touch $VALJUNDFAIL
-                exit 2
+				if [ ! -f $VALJUNDFAIL ]
+					then
+						echo "Väljundfaili ei saa avada!"
+					exit 2
+				fi
         fi
 
-#Väljundfaili esimesele reale kirjutatakse 3. parameetrina antud string. 
-#        echo $OTSITAV > $VALJUNDFAIL
+#Skript leiab kõik failid parameetriks küsitud kataloogist, millele parameetriks küsitud kasutajanimega kasutajal on kirjutamisõigus.
+	find $KATALOOG \( -type f \) \
+		\( \
+			\( -user $KASUTAJANIMI -perm -u=w \) -or \
+			\( -group $KASUTAJANIMI -perm -g=w \) -or \
+			-perm -o=w \
+		\) \
+	-print > $VALJUNDFAIL
 
-#Skript loeb sisendfailist failide ja kataloogide nimekirja ning otsib nimekirja failide ja kataloogide seest (rekursiivselt) OTSITAVAT STRINGI.
-#	while read F
-#		do
-#			if grep -qrnw "$OTSITAV" $F
-#				then
-#					echo $F",OLEMAS" >> $VALJUNDFAIL
-#				else
-#					echo $F",POLE OLEMAS" >> $VALJUNDFAIL
-#			fi
-#		done <$SISENDFAIL
+##############################################
+##############################################
+# Järgnev langeb arvestustöö skoobist välja, #
+# kuid on äärmiselt kasulik võimalus sellise #
+# skripti puhul. Vajadusel saab arendada #####
+# optsioonivõimaluse lisaparameetri näol. ####
+##############################################
+##############################################
+#Skript leiab kõik failid ja kaustad parameetriks küsitud kataloogist, millele parameetriks küsitud kasutajanimega kasutajal on kirjutamiseõigus.
+#	find $KATALOOG \( -type f -or -type d \) \
+#		\( \
+#			\( -user $KASUTAJANIMI -perm -u=w \) -or \
+#			\( -group $KASUTAJANIMI -perm -g=w \) -or \
+#			-perm -o=w \
+#		\) \
+#	-print
+##############################################
+##############################################
 
 #Tulemuse raport kasutajale
-#echo -e "Faili-/katalooginimekiri: \e[1;4m$SISENDFAIL\e[0m || OTSITAV: \e[1;4m$OTSITAV\e[0m || TULEMUSED : \e[1;4m$VALJUNDFAIL\e[0m"
+echo -e "KATALOOG: \e[1;4m$KATALOOG\e[0m || KASUTAJANIMI: \e[1;4m$KASUTAJANIMI\e[0m || TULEMUSED : \e[1;4m$VALJUNDFAIL\e[0m"

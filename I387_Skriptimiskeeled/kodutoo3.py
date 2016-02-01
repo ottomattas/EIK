62db490a5dd147172af7a83c5ac8f8c0967adc6f
ottomattas <otto.mattas@gmail.com>
2016-02-01 22:37:35 +0200

    v1.1 Optimaalsuse tõstmiseks toodud väljundfaili avamine tsüklist välja

--- I387_Skriptimiskeeled/kodutoo3.py
+++ I387_Skriptimiskeeled/kodutoo3.py
@@ -6,6 +6,7 @@
 #Vers. tabel
 # v0.1 Esimesed baaselemendid
 # v1.0 Valmistoode
+# v1.1 Optimaalsuse tõstmiseks toodud väljundfaili avamine tsüklist välja
 
 import sys
 import urllib2
@@ -16,45 +17,44 @@ if len(sys.argv) != 3:
 	print "Kasuta programmi:", sys.argv[0], "[sisendfail] [väljundfail]"
 	sys.exit(1)
 
-#print "Argumentide arv on: ", len(sys.argv)
-
-#Ava SISENDFAIL ning eralda tekstist URLid / stringid (lisaks kontroll selle faili avamise võimalikkusele)
+#Väljundfaili avamine optimaalsuse tagamiseks (lisaks kontroll selle faili kirjutamise võimalikkusele)
 try:
-	with open(sys.argv[1], 'r') as input_file:
-		for line in input_file.readlines():
-			line = line.split(',')
-			#Leiame URLi
-			line_url = line[0]
-			#Leiame otsitava stringi
-			line_string = line[1].strip()
-			#Ava URL ning leia lähtekoodist (HTMLi) otsitav string (lisaks kontroll selle URLi avamise võimalikkusele)
-			try:
-				url = urllib2.urlopen(line_url)
-				sisu = url.read()
-				tulemus = sisu.find(line_string)
-				#Tulemus, kui string leiti
-				if tulemus > 0:
-					tulemus = "JAH"
-				#Tulemus, kui stringi ei leitud
-				else:
-					tulemus = "EI"
-				#Väljundfaili kirjutatava defineerimine
-				valjund = line_url + "," + line_string + "," + tulemus
-				#Väljundfaili kirjutamine (lisaks kontroll selle faili kirjutamise võimalikkusele)
+	output_file = open(sys.argv[2], 'a')
+	#Ava SISENDFAIL ning eralda tekstist URLid / stringid (lisaks kontroll selle faili avamise võimalikkusele)
+	try:
+		with open(sys.argv[1], 'r') as input_file:
+			for line in input_file.readlines():
+				line = line.split(',')
+				#Leiame URLi
+				line_url = line[0]
+				#Leiame otsitava stringi
+				line_string = line[1].strip()
+				#Ava URL ning leia lähtekoodist (HTMLi) otsitav string (lisaks kontroll selle URLi avamise võimalikkusele)
 				try:
-					with open(sys.argv[2], 'a') as output_file:
-						#Teeme väljundfaili kirjutatavast stringi
-						valjund = str(valjund)
-						#Kirjutame tulemused faili eraldi ridadele
-						output_file.write(valjund + "\n")
-				except IOError:
-					print "Väljundfaili ei saa kirjutada..."
-					sys.exit(3)
-			except Exception:
-				print "URLi", line_url, "ei saa avada!"
+					url = urllib2.urlopen(line_url)
+					sisu = url.read()
+					tulemus = sisu.find(line_string)
+					#Tulemus, kui string leiti
+					if tulemus > 0:
+						tulemus = "JAH"
+					#Tulemus, kui stringi ei leitud
+					else:
+						tulemus = "EI"
+					#Väljundfaili kirjutatava defineerimine
+					valjund = line_url + "," + line_string + "," + tulemus
+					#Teeme väljundfaili kirjutatavast stringi
+					valjund = str(valjund)
+					#Kirjutame tulemused väljundfaili eraldi ridadele
+					output_file.write(valjund + "\n")
+				except Exception:
+					print "URLi", line_url, "ei saa avada!"
+	except IOError:
+		print "Sisendfaili ei saa avada..."
+		sys.exit(2)
+	output_file.close()
 except IOError:
-	print "Sisendfaili ei saa avada..."
-	sys.exit(2)
+	print "Väljundfaili ei saa kirjutada..."
+	sys.exit(3)
 
 #Tulemuse raport kasutajale
 print "Programm lõpetas töö."
